53089c6efc7b9e7f9852e1ff63d848f4444bd593
Cody Engel <cengel815@gmail.com>
2022-02-12 10:09:59 -0600

    Delete iOS Configurations
    Deleted the iOS configurations from pokeModel as it's not currently relevant.

--- pokeModel/build.gradle.kts
+++ pokeModel/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("multiplatform")
-//    kotlin("native.cocoapods")
     kotlin("plugin.serialization")
     id("com.android.library")
 }
@@ -9,18 +8,6 @@ version = ApplicationVersion
 
 kotlin {
     android()
-//    iosX64()
-//    iosArm64()
-//    //iosSimulatorArm64() sure all ios dependencies support this target
-//
-//    cocoapods {
-//        summary = "Some description for the Shared Module"
-//        homepage = "Link to the Shared Module homepage"
-//        ios.deploymentTarget = "14.1"
-//        framework {
-//            baseName = "pokeModel"
-//        }
-//    }
     
     sourceSets {
         val commonMain by getting {
@@ -37,30 +24,6 @@ kotlin {
             }
         }
         val androidMain by getting
-//        val androidTest by getting {
-//            dependencies {
-//                implementation(kotlin("test-junit"))
-//                implementation("junit:junit:4.13.2")
-//            }
-//        }
-//        val iosX64Main by getting
-//        val iosArm64Main by getting
-//        //val iosSimulatorArm64Main by getting
-//        val iosMain by creating {
-//            dependsOn(commonMain)
-//            iosX64Main.dependsOn(this)
-//            iosArm64Main.dependsOn(this)
-//            //iosSimulatorArm64Main.dependsOn(this)
-//        }
-//        val iosX64Test by getting
-//        val iosArm64Test by getting
-//        //val iosSimulatorArm64Test by getting
-//        val iosTest by creating {
-//            dependsOn(commonTest)
-//            iosX64Test.dependsOn(this)
-//            iosArm64Test.dependsOn(this)
-//            //iosSimulatorArm64Test.dependsOn(this)
-//        }
     }
 }
 
