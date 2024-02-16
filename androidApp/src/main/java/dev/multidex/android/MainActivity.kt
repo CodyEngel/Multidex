5aaa0662dd3a5fc3427b6afa134dfa7847378df8
Cody Engel <cengel815@gmail.com>
2024-02-16 16:47:56 -0700

    Remove deprecated and unnecessary launchWhenCreated

--- androidApp/src/main/java/dev/multidex/android/MainActivity.kt
+++ androidApp/src/main/java/dev/multidex/android/MainActivity.kt
@@ -3,7 +3,6 @@ package dev.multidex.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,18 +13,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launchWhenCreated {
-            setContent {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "pokemonList") {
-                    composable("pokemonList") {
-                        PokemonListScreen(navController = navController)
-                    }
-                    composable("pokemon/{pokemonId}") { entry ->
-                        entry.arguments?.getString("pokemonId")
-                            ?.let { PokemonDetailsScreen(it.toInt()) }
-                            ?: throw IllegalStateException("Pokemon ID Not Available")
-                    }
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "pokemonList") {
+                composable("pokemonList") {
+                    PokemonListScreen(navController = navController)
+                }
+                composable("pokemon/{pokemonId}") { entry ->
+                    entry.arguments?.getString("pokemonId")
+                        ?.let { PokemonDetailsScreen(it.toInt()) }
+                        ?: throw IllegalStateException("Pokemon ID Not Available")
                 }
             }
         }
